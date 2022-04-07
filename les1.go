a0b995c71e390e9a7d195dc5db929986d94c8b38
neonoren <neonoren@gmail.com>
2022-04-07 23:10:50 +0500

    Initial commit

---
+++ les1.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	ages := make(map[string]int)
+	ages["Андрей"] = 38
+	ages["Ирина"] = 35
+	ages["Максим"] = 7
+	ages["Василиса"] = 3
+
+	for key, value := range ages {
+		fmt.Printf("%s - %d\n", key, value)
+
+	}
+	fmt.Printf("Максиму сейчас %d лет", ages["Максим"])
+}
